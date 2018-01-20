00de7b0c337e457be3be869c4a4d87e0d12d3a22
zeluisfdias <34245226+zeluisfdias@users.noreply.github.com>
2018-01-20 21:38:26 +0000

    Adição das Classes Sala de Processsamento e Formigueiro Em falta: Implementação das mesmas

--- src/Classes/Sala.java
+++ src/Classes/Sala.java
@@ -5,6 +5,7 @@
  */
 package Classes;
 
+import Collections.ArrayUnorderedList;
 import recursos.exceptions.ElementNotFoundException;
 import recursos.exceptions.EmptyCollectionException;
 import recursos.interfaces.IFormiga;
@@ -12,22 +13,27 @@ import recursos.interfaces.ISala;
 import recursos.interfaces.collections.UnorderedListADT;
 
 /**
- * Classe responsavel pela Gestão de Salas no Formigueiro 
+ * Classe responsavel pela Gestão de Salas no Formigueiro
+ *
  * @author Zé Luís
  */
-public class Sala implements ISala{
-
+public class Sala implements ISala {
+    
     private int Id;
     private String Descricao;
     private int x;
     private int y;
+    
+    private ArrayUnorderedList formigasSala; //array de formigas na sala
 
     /**
-     * Construtor que cria uma nova sala, com os varios atributos associados a classe @link Sala
+     * Construtor que cria uma nova sala, com os varios atributos associados a
+     * classe @link Sala
+     *
      * @param Id identificador da sala
      * @param Descricao descricao da sala
      * @param x coordenada x no plano bidimensional
-     * @param y  coordenada y no plano bidimensional
+     * @param y coordenada y no plano bidimensional
      */
     public Sala(int Id, String Descricao, int x, int y) {
         this.Id = Id;
@@ -36,67 +42,78 @@ public class Sala implements ISala{
         this.y = y;
     }
 //Metodos de acesso as variaveis privadas da classe
+
     @Override
     public int getId() {
         return Id;
     }
+
     @Override
     public void setId(int Id) {
         this.Id = Id;
     }
+
     @Override
     public String getDescricao() {
         return Descricao;
     }
+
     @Override
     public void setDescricao(String Descricao) {
         this.Descricao = Descricao;
     }
+
     @Override
     public int getX() {
         return x;
     }
+
     @Override
     public void setX(int x) {
         this.x = x;
     }
+
     @Override
     public int getY() {
         return y;
     }
+
     @Override
     public void setY(int y) {
         this.y = y;
     }
-    
+
     // Metodos de Gestão das Estruturas de Dados
-  
     /**
      * Acrescenta uma formiga a lista de formigas presentes nesta sala
+     *
      * @param newFormiga Objecto Formiga
      */
     @Override
     public void entraFormiga(IFormiga newFormiga) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        formigasSala.addToFront(newFormiga); //adiciona a nova formiga ao array de formigas
     }
 
-    
     /**
      * Retira uma formiga das várias formigas existentes nesta sala
+     *
      * @param i posicao da formiga a eliminar da sala
      * @return formiga que saiu
-     * @throws EmptyCollectionException lancada esta excepcao se nao existem nenhuma formiga na sala
-     * @throws ElementNotFoundException  lancada esta excepcao se nao existe a formiga que foi passada como parametro
+     * @throws EmptyCollectionException lancada esta excepcao se nao existem
+     * nenhuma formiga na sala
+     * @throws ElementNotFoundException lancada esta excepcao se nao existe a
+     * formiga que foi passada como parametro
      */
     @Override
     public IFormiga saiFormiga(int i) throws EmptyCollectionException, ElementNotFoundException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return null;
+       
     }
 
-    
     /**
      * Retorna a lista de formigas existentes numa sala
-     * @return  unorderedlist com todas a formigas existentes na sala.
+     *
+     * @return unorderedlist com todas a formigas existentes na sala.
      */
     @Override
     public UnorderedListADT<IFormiga> listaFormigas() {
