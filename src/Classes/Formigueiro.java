0071a8b4630267d77714b426bbd09da9c046f6b1
zeluisfdias <34245226+zeluisfdias@users.noreply.github.com>
2018-01-20 20:10:10 +0000

    Adição das Classes Sala de Processsamento e Formigueiro Em falta: Implementação das mesmas

---
+++ src/Classes/Formigueiro.java
@@ -0,0 +1,151 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Classes;
+
+import java.util.Iterator;
+import recursos.exceptions.ElementNotFoundException;
+import recursos.interfaces.IComida;
+import recursos.interfaces.IFormiga;
+import recursos.interfaces.IFormigueiro;
+import recursos.interfaces.IPair;
+import recursos.interfaces.IProcessamento;
+import recursos.interfaces.ISala;
+import recursos.interfaces.ISilo;
+import recursos.interfaces.ITunel;
+
+/**
+ *
+ * @author Zé Luís
+ */
+public class Formigueiro implements IFormigueiro{
+
+    private Sala salaEntrada;
+
+    /**
+     * Construtor para criar um novo formigueiro.
+     * @param salaEntrada 
+     */
+    public Formigueiro(Sala salaEntrada) {
+        this.salaEntrada = salaEntrada;
+    }
+    
+    
+    @Override
+    public ISala getEntrada() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void setEntrada(ISala isala) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<ISala> iterator() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<ISala> iteratorBFS(ISala isala) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<ISala> iteratorBFS() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void addSala(ISala isala) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void ligaSala(ISala isala, ISala isala1, int i) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getMaxY() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int getMaxX() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<IPair<ISala, ITunel>> ligacoesDe(ISala isala) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ISala getSalaFormiga(int i) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IFormiga getFormiga(int i) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ISala getSala(int i) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean vizinhos(ISala isala, ISala isala1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<ISala> iteratorShortestPath(int i, int i1) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<ISala> iteratorMoveFormigaShortestPath(int i, int i1) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Iterator<ISala> iteratorCarregaEMoveFormigaShortestPath(int i, int i1) throws ElementNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int custoDoCaminho(Iterator<ISala> itrtr) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IFormiga criaFormiga(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IComida criaComida(int i, int i1) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ISala criaSala(int i, String string, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public ISilo criaSilo(int i, String string, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public IProcessamento criaProcessamento(int i, String string, int i1, int i2) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
--- src/Classes/SalaProcessamento.java
+++ src/Classes/SalaProcessamento.java
@@ -7,33 +7,39 @@ package Classes;
 
 import java.util.Iterator;
 import recursos.exceptions.EmptyCollectionException;
+import recursos.exceptions.ProcessedException;
 import recursos.interfaces.IComida;
-import recursos.interfaces.ISilo;
+import recursos.interfaces.IProcessamento;
+
 
 /**
- * SubClasse Silo da Classe Pai Sala, usada para representar um Silo
- * Usa os atributos da Classe Pai
- * Para o empilhamento de comida teremos que usar Stack pois o Silo é uma estrutura vertical (Stack) que as formigas depositam
- * comida uma em cima das outra. 
- * Quando eliminam é a primeira do topo que sai.
- * O silo não tem limite de espaço 
+ * Classe SalaProcessamnto é uma subclass de Sala que indica uma sala em que as formigas 
+ * processam a comida, que funciona como uma linha de producao. Ou seja a nova comida é depositada no fim da linha de producao.
  * 
- * FILO - First In First Out
+ * Não há limite de espaço. Quando uma formiga sai da Sala ou a proxima comida tem tamanho 1.
  * @author Zé Luís
  */
-public class Silo extends Sala implements ISilo {
-
-    public Silo(int Id, String Descricao, int x, int y) {
+public class SalaProcessamento extends Sala implements IProcessamento{
+    
+    /**
+     * Construtor para criar uma sala de processamento para o deposito de comida como linha de producao
+     * em Queue, com a metodologia FIFO
+     * @param Id
+     * @param Descricao
+     * @param x
+     * @param y 
+     */
+    public SalaProcessamento(int Id, String Descricao, int x, int y) {
         super(Id, Descricao, x, y);
     }
 
     @Override
-    public void guardaComida(IComida ic) {
+    public void acrescentaComida(IComida ic) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
     @Override
-    public IComida retiraComida() throws EmptyCollectionException {
+    public IComida getProximaComida() throws EmptyCollectionException, ProcessedException {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
 
